d7cc4fed3a768353ee4d991b1d375e08a72fcbe8
Max Rosin <git@hackrid.de>
2019-06-20 21:28:58 +0200

    commit current state

--- main.py
+++ main.py
@@ -34,17 +34,13 @@ logging.getLogger('telegram').setLevel(logging.INFO)
 logging.getLogger('requests').setLevel(logging.INFO)
 
 logging_format = '[%(asctime)s: %(levelname)s/%(name)s] %(message)s'
-formatter = logging.Formatter(logging_format)
 logging.basicConfig(level=logging.INFO, format=logging_format)
 
-logger = logging.getLogger('FrundenBot')
-logger_fh = logging.handlers.TimedRotatingFileHandler('logs/frundenbot.log', when='midnight', backupCount=360)
-logger_fh.setLevel(logging.DEBUG)
-logger_fh.setFormatter(formatter)
-logger.addHandler(logger_fh)
+LOGGER = logging.getLogger(__name__)
 
 TOKEN = os.environ.get('TELEGRAM_BOT_AUTH_TOKEN')
-
+ADMIN = int(os.environ.get('TELEGRAM_BOT_ADMIN'))
+LIST_OF_ADMINS = [ int(x) for x in os.environ.get('TELEGRAM_BOT_ADMINS').split(',') ]
 
 def __log_incomming_messages(bot, update):
         chat = update.message.chat
@@ -58,13 +54,12 @@ def __log_incomming_messages(bot, update):
             target_chat += ' %s' % chat.last_name
           if chat.username:
             target_chat += ' (%s)' % chat.username
-        logger.info('In:  %s: %s' % (target_chat, update.message.text))
-
+        LOGGER.info('In:  %s: %s' % (target_chat, update.message.text))
 
 def refresh_cache(bot, job):
     global cache
     try:
-        logger.info('Refresh cache')
+        LOGGER.debug('Refresh cache')
         r = requests.get('https://watchyour.freitagsrunde.org')
         r.raise_for_status()
         if 'Wir sind fuer dich da!' in r.text:
@@ -72,7 +67,9 @@ def refresh_cache(bot, job):
         else:
             cache = emojize(':red_circle: Leider haben wir gerade zu.', use_aliases=True)
     except Exception as e:
-        logger.error(e)
+        FRUNDE_OPEN.set(-1)
+        cache = emojize('Sorry, ich weiß es nicht! :confused:', use_aliases=True)
+        LOGGER.error(e)
 
 
 def start(bot, update):
@@ -91,13 +88,54 @@ def inline(bot, update):
             input_message_content = InputTextMessageContent(cache)
         )
     )
-    logger.info('Inline Query')
+    LOGGER.info('Inline Query')
     bot.answerInlineQuery(update.inline_query.id, results)
 
 
 def is_open(bot, update):
     __log_incomming_messages(bot,update)
-    bot.sendMessage(chat_id=update.message.chat_id, text=cache)
+    bot.sendMessage(chat_id=update.message.chat_id, text='{}\nÜbrigens kannst du mit /mate nachgucken, ob es noch Getränke gibt.'.format(cache))
+
+
+WHOAMI_TIME = Summary('frunde_whoami_seconds', 'Time spent executing /whoami handler')
+@WHOAMI_TIME.time()
+def whoami(bot, update):
+    __log_incomming_messages(bot,update)
+    bot.sendMessage(chat_id=update.message.chat_id, text='You are: {} ({})'.format(update.message.from_user.name, update.message.chat_id))
+    LOGGER.info('This is: {} ({})'.format(update.message.from_user.name, update.message.chat_id))
+
+
+GET_DRINKS_TIME = Summary('frunde_get_drinks_seconds', 'Time spent executing /mate (/drinks) handler')
+@GET_DRINKS_TIME.time()
+def get_drinks(bot, update):
+    __log_incomming_messages(bot,update)
+    try:
+        with open('/var/frunde/frunde_drinks.txt', 'r') as file:
+            drinks = file.read()
+    except Exception as e:
+        drinks = emojize('Uhm, das weiß ich nicht. :confused:', use_aliases=True)
+        LOGGER.error(e)
+    bot.sendMessage(chat_id=update.message.chat_id, text=drinks)
+
+
+SET_DRINKS = Summary('frunde_set_drinks_seconds', 'Time spent executing /set_mate handler')
+@SET_DRINKS.time()
+def set_drinks(bot, update, args):
+    __log_incomming_messages(bot,update)
+    if update.message.chat_id in LIST_OF_ADMINS:
+        mate_message = ' '.join(args)
+        LOGGER.info('New mate message: {}'.format(mate_message))
+        try:
+            with open('/var/frunde/frunde_drinks.txt', 'w+') as file:
+                file.write('{}\n(Aktualisiert: {})'.format(mate_message, time.strftime('%d.%m.%Y um %H:%M')))
+                result = 'Neuer Matepegel:\n{}'.format(mate_message)
+        except Exception as e:
+            result = emojize('Uhm, das hat nicht geklappt. :confused:', use_aliases=True)
+            LOGGER.error(e)
+        bot.sendMessage(chat_id=update.message.chat_id, text=result)
+        bot.sendMessage(chat_id=ADMIN, text='Neuer Matepegel von {} ({}):\n{}'.format(update.message.from_user.name, update.message.chat_id, result))
+    else:
+        bot.sendMessage(chat_id=update.message.chat_id, text=emojize(':poop: Nö :poop:', use_aliases=True))
 
 
 if __name__ == '__main__':
